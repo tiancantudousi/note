1d49b045096efed8524210efef40efcbd1055faa
tiancantudousi <30852480+tiancantudousi@users.noreply.github.com>
2021-12-07 07:53:49 +0800

    2

--- js/reg/string1.js
+++ js/reg/string1.js
@@ -1,4 +1,4 @@
-/*
+/* https://www.runoob.com/js/js-strings.html
   1.  substr() 方法可在字符串中抽取从 开始 下标开始的指定数目的字符。注意： substr() 方法不会改变源字符串。
   语法 string.substr(start,length)
   参数 start 必需。要抽取的子串的起始下标。必须是数值。如果是负数，那么该参数声明从字符串的尾部开始算起的位置。也就是说，-1 指字符串中最后一个字符，-2 指倒数第二个字符，以此类推。
